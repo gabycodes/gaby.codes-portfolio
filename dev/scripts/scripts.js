7851f7ca5ce1266ff90f5d674537117fa59b46f1
Gaby Kupfer <g.kupfer@hotmail.com>
2017-12-08 16:50:25 -0500

    Added animations as you scroll through using wow.js, added smoothScroll for nav items, added more styling.

--- dev/scripts/scripts.js
+++ dev/scripts/scripts.js
@@ -1,44 +1,73 @@
-$('.main-carousel').flickity({
-    // options
-    cellAlign: 'left',
-    contain: true,
-    wrapAround: true
-});
+$(document).ready(function () {
 
-$(function () {
-    $(".typed").typed({
-        strings: ["is friendly and positive.", 
-        "codes with accessibility in mind.",
-        "keeps things organized", 
-        "is efficient and effective."],
-        stringsElement: null,
-        // typing speed
-        typeSpeed: 35,
-        // time before typing starts
-        startDelay: 1900,
-        // backspacing speed
-        backSpeed: 30,
-        // time before backspacing
-        backDelay: 600,
-        // loop
-        loop: false,
-        // false = infinite
-        loopCount: 5,
-        // show cursor
-        showCursor: false,
-        // character for cursor
-        cursorChar: "|",
-        // attribute to type (null == text)
-        attr: null,
-        // either html or text
-        contentType: 'html',
-        // call when done callback function
-        callback: function () { },
-        // starting callback function before each string
-        preStringTyped: function () { },
-        //callback for every typed string
-        onStringTyped: function () { },
-        // callback for reset
-        resetCallback: function () { }
+    // Add smooth scrolling to all links
+    $("a").on('click', function (event) {
+
+        // Make sure this.hash has a value before overriding default behavior
+        if (this.hash !== "") {
+            // Prevent default anchor click behavior
+            event.preventDefault();
+
+            // Store hash
+            var hash = this.hash;
+
+            // Using jQuery's animate() method to add smooth page scroll
+            // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
+            $('html, body').animate({
+                scrollTop: $(hash).offset().top
+            }, 1100, function () {
+
+                // Add hash (#) to URL when done scrolling (default click behavior)
+                window.location.hash = hash;
+            });
+        } // End if
     });
+
+    $('.main-carousel').flickity({
+        // options
+        cellAlign: 'left',
+        contain: true,
+        wrapAround: true
+    });
+
+    $(function () {
+        $(".typed").typed({
+            strings: ["is friendly and positive.", 
+            // "codes with accessibility in mind.",
+            // "keeps things organized", 
+            "is efficient and effective."],
+            stringsElement: null,
+            // typing speed
+            typeSpeed: 35,
+            // time before typing starts
+            startDelay: 400,
+            // backspacing speed
+            backSpeed: 30,
+            // time before backspacing
+            backDelay: 600,
+            // loop
+            loop: true,
+            // false = infinite
+            loopCount: false,
+            // show cursor
+            showCursor: false,
+            // character for cursor
+            cursorChar: "|",
+            // attribute to type (null == text)
+            attr: null,
+            // either html or text
+            contentType: 'html',
+            // call when done callback function
+            callback: function () { },
+            // starting callback function before each string
+            preStringTyped: function () { },
+            //callback for every typed string
+            onStringTyped: function () { },
+            // callback for reset
+            resetCallback: function () { }
+        });
+    });
+
+    new WOW().init();
+
 });
